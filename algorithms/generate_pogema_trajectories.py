cccf5bf3ee591b147b819c9ff0c10ecd99e5a2cd
Zoya Volovikova <babycar@pop-os.localdomain>
2022-11-18 14:54:17 +0300

    more less stable dt for pogema

--- algorithms/generate_pogema_trajectories.py
+++ algorithms/generate_pogema_trajectories.py
@@ -1,51 +1,105 @@
-sys.path.append("./pogema-appo")
-print("--------------")
-import os
-cwd = os.getcwd()
-print(cwd)
-print("--------------")
-from pomapf.wrappers import MatrixObservationWrapper
-import gym
-# noinspection PyUnresolvedReferences
-import pomapf
-from pogema import GridConfig
-from gym.wrappers import FrameStack
-
-from torch import nn
-import torch 
+from collections import defaultdict
 import numpy as np
+from sklearn.model_selection import train_test_split
+import d3rlpy
 
-def load_pogema_trajectories(dataset):
-    #dataset = gym.make(env_name).get_dataset()
-    dataset = d3rlpy.dataset.MDPDataset.load("./algorithms/mixed_dataset_10_00_5000.h5")
-    train_episodes, test_episodes = train_test_split(dataset, test_size=0.2, shuffle = True)
+from dt import discounted_cumsum
+from collections import Counter
+
+def distance_to_goal(obs):
+    ax, ay = np.asarray(obs[1].shape)//2
+    #print(obs[2])
+    #exit()
+    if np.sum(obs[2]) == 1:
+        x,y = np.where(obs[2]==1)
+        return ((ax-x)**2 + (ay-y)**2)**0.5
+    return 1000
+
+def episode_rewards_by_obs(episode_observation):
+    min_dist = 1000
+    new_rewards = []
+    last_screen = np.zeros_like(episode_observation[0][0])
+    for obs in episode_observation:
+        dist = distance_to_goal(obs[-3:])
+        reward = -0.0035
+        if dist < min_dist:
+            min_dist = dist
+            reward = 0.1
+        new_rewards.append(reward)
+    new_rewards[-1]=1
+    return new_rewards
+
+def load_pogema_trajectories(use_image=True):
+    dataset = d3rlpy.dataset.MDPDataset.load("./mixed_dataset_10_00_5000.h5")
+    train_episodes, test_episodes = train_test_split(dataset, test_size=0.2, shuffle=True)
     traj, traj_len = [], []
 
     data_, episode_step = defaultdict(list), 0
     episode_data = dict()
     traj = []
     traj_len = []
-    full_data = {"observations":[]}
+    full_data = {"observations": []}
+    episode_data = dict()
     for i in range(len(dataset)):
-        episode_data["observations"] = dataset[i].observations.reshape(-1,12, 21,21)
-      #  raise Exception(episode_data["observations"].shape)
-        full_data["observations"] +=  dataset[i].observations.tolist()
-        episode_data["actions"] = dataset[i].actions
-        episode_data["rewards"] = dataset[i].rewards
-        episode_data["returns"] = discounted_cumsum(
-                episode_data["rewards"], gamma=1
-             )
-        episode_len = len(dataset[i].rewards)
+        #mask = np.where(dataset[i].actions == 0)
+        mask = np.where(dataset[i].actions >= 0)
+        # print("Zero indexes: ", mask)
+        # print("Other: ", mask2)
+        # print("-------------")
+        # continue
+        episode = dataset[i]
+        episode_len = len(episode.rewards[mask])
+        if episode_len < 20: continue
+        new_rewards = np.asarray(episode_rewards_by_obs(episode.observations))
+        for j in range(episode_len,20,-3):
+            i = j-20
+            if use_image:
+                episode_data["observations"] = episode.observations[mask][i:i+20].reshape(-1, 12, 21, 21)
+                full_data["observations"] += episode.observations[mask][i:i+20].tolist()
+            else:
+                episode_data["observations"] = episode.observations[mask][i:i+20].reshape(-1, 12 * 21 * 21)
+                full_data["observations"] += episode.observations[mask][i:i+20].reshape(-1, 12 * 21 * 21).tolist()
+            episode_data["actions"] = episode.actions[mask][i:i+20].reshape(-1, 1)
+            episode_data["rewards"] = new_rewards[mask][i:i+20]
+            reward_mask = np.where(episode_data["actions"].ravel() == 0)
+            episode_data["rewards"][reward_mask] = -0.05
+         #   episode_data["rewards"][-1] = 1.5
+            episode_data["returns"] = episode_data["rewards"]
+            episode_len = len(episode.rewards[mask][i:i+20])
+            traj.append(episode_data)
+            traj_len.append(episode_len)
+            episode_data = dict()
 
-        traj.append(episode_data)
-        traj_len.append(episode_len)
-        
-    full_data["observations"] =  np.asarray(full_data["observations"])
-  #  print(full_data["observations"].shape)
-    # needed for normalization, weighted sampling, other stats can be added also
+    print("Trajectories statistics: ")
+    print("Mean: ", np.mean(traj_len))
+    print("Median: ", np.median(traj_len))
+    print("Max: ", np.max(traj_len))
+    print("Min: ", np.min(traj_len))
+    full_data["observations"] = np.asarray(full_data["observations"])
     info = {
         "obs_mean": full_data["observations"].mean(0, keepdims=True),
         "obs_std": full_data["observations"].std(0, keepdims=True) + 1e-6,
         "traj_lens": np.array(traj_len),
     }
     return traj, info
+
+
+if __name__ == "__main__":
+    traj, _ = load_pogema_trajectories(use_image=False)
+    print(traj[0]['observations'].shape)
+    print(traj[0]['actions'].shape)
+    print(traj[0]['returns'].shape)
+    print(traj[0]['rewards'].shape)
+
+    from collections import Counter
+
+    act = traj[0]['actions'].ravel().tolist()
+    print(Counter(act))
+    act = traj[1]['actions'].ravel().tolist()
+    print(Counter(act))
+    act = traj[2]['actions'].ravel().tolist()
+    print(Counter(act))
+    act = traj[3]['actions'].ravel().tolist()
+    print(Counter(act))
+    act = traj[53]['actions'].ravel().tolist()
+    print(Counter(act))
--- algorithms/pogema_wrappers.py
+++ algorithms/pogema_wrappers.py
@@ -1,10 +1,4 @@
 import numpy as np
-sys.path.append("./pogema-appo")
-print("--------------")
-import os
-cwd = os.getcwd()
-print(cwd)
-print("--------------")
 from pomapf.wrappers import MatrixObservationWrapper
 import gym
 # noinspection PyUnresolvedReferences
@@ -13,16 +7,45 @@ from pogema import GridConfig
 from gym.wrappers import FrameStack
 
 
-class ObsActionWrapper(gym.Wrapper):
+class RewardLogger(gym.Wrapper):
+    total_episodes = 0
+
+    def __init__(self, env, logger):
+        super().__init__(env)
+        self.total_reward = 0
+        self.episodes = 0
+        self.reward = 0
+        self.logger = logger
+
+    def reset(self, **kwargs):
+        self.total_episodes += 1
+        self.episodes += 1
+        if self.total_episodes % 10 == 0:
+            self.logger.log({"env_wrapper_reward": self.total_reward / self.episodes,
+                             "total_episodes": self.total_episodes})
+            self.total_reward = 0
+            self.episodes = 0
+        # else:
+        self.logger.log({"reward": self.reward, "total_episodes": self.total_episodes})
+        self.reward = 0
+        return self.env.reset()
+
+    def get_normalized_score(self, score):
+        return score
+
+    def step(self, action):
+        observations, reward, done, info = self.env.step(action)
+        self.total_reward += reward
+        self.reward += reward
+        return observations, reward, done, info
+
+
+class Obs1DActionWrapper(gym.Wrapper):
     def __init__(self, env):
         super().__init__(env)
-      #  print(len(self.config.MOVES))
-        #self.action_space = gym.spaces.Box(0.0, 1.0, shape = (1,))
         full_size = self.config.obs_radius * 2 + 1
-        #obs for img 
-        self.observation_space = gym.spaces.Box(0.0, 1.0, shape=(3,full_size, full_size,))
-        #obs for line 
-        #self.observation_space = gym.spaces.Box(0.0, 1.0, shape=(3*full_size*full_size,))
+        # obs for line
+        self.observation_space = gym.spaces.Box(0.0, 1.0, shape=(3 * full_size * full_size,))
 
     def reset(self, **kwargs):
         obs = MatrixObservationWrapper.to_matrix(self.env.reset())
@@ -32,15 +55,15 @@ class ObsActionWrapper(gym.Wrapper):
             ob = obs[i]['obs']
             ob = np.asarray(ob)
             ob = ob.astype(np.uint8)
-            #obs_.append(ob.reshape(-1,))
-            obs_.append(ob)
+            obs_.append(ob.reshape(-1, ))
         return np.asarray(obs_)
-    
-    def get_normalized_score(self,score):
+
+    def get_normalized_score(self, score):
         return score
 
     def step(self, action):
-      #  print("Actions for pogema: ", action)
+        #  print("Actions for pogema: ", action)
+     #   if env.config.num_agents
         action = [action]
         action = np.asarray(action).astype(int)
         observations, reward, done, info = self.env.step(action)
@@ -51,77 +74,183 @@ class ObsActionWrapper(gym.Wrapper):
             ob = obs[i]['obs']
             ob = np.asarray(ob)
             ob = ob.astype(np.uint8)
-           # obs_.append(ob.reshape(-1,))
-            obs_.append(ob)
+            obs_.append(ob.reshape(-1, ))
         return np.asarray(obs_), reward[0], done[0], info[0]
 
-class RewardLogger(gym.Wrapper):
-    total_episodes = 0
+
+class Obs2DActionWrapper(gym.Wrapper):
     def __init__(self, env):
         super().__init__(env)
-        self.total_reward = 0
-        self.episodes = 0
-        self.reward = 0
-        
+        full_size = self.config.obs_radius * 2 + 1
+        # obs for img
+        self.observation_space = gym.spaces.Box(0.0, 1.0, shape=(3, full_size, full_size,))
+
     def reset(self, **kwargs):
-        self.total_episodes += 1
-        self.episodes += 1
-        
-        if self.total_episodes%10 == 0:
-            wandb.log({"env_wrapper_reward": self.total_reward/self.episodes,
-                       "total_episodes":self.total_episodes})
-            self.total_reward = 0
-            self.episodes = 0
-       # else:
-        wandb.log({"reward":self.reward, "total_episodes":self.total_episodes})
-        self.reward = 0
-        return self.env.reset()
-    
-    def get_normalized_score(self,score):
+        #  self.env.reset()
+        obs = MatrixObservationWrapper.to_matrix(self.env.reset())
+
+        agents = len(obs)
+        obs_ = []
+        for i in range(agents):
+            ob = obs[i]['obs']
+            ob = np.asarray(ob)
+            ob = ob.astype(np.uint8)
+            obs_.append(ob)
+        return np.asarray(obs_)
+
+    def get_normalized_score(self, score):
         return score
 
     def step(self, action):
-      #  print("Actions for pogema: ", action)
-
+     #   print("Actions for pogema: ", action)
+        if not isinstance(action, list):
+            action = [action]
+        action = np.asarray(action).astype(int)
         observations, reward, done, info = self.env.step(action)
-        self.total_reward += reward       
-        self.reward += reward
-        return  observations, reward, done, info
-    
-def reshape_obs(obs):
-    #in - N agents 3 21 21
+        obs = MatrixObservationWrapper.to_matrix(observations)
+        agents = len(obs)
+        obs_ = []
+        for i in range(agents):
+            ob = obs[i]['obs']
+            ob = np.asarray(ob)
+            ob = ob.astype(np.uint8)
+            # obs_.append(ob.reshape(-1,))
+            obs_.append(ob)
+        return np.asarray(obs_), reward[0], done[0], info[0]
+
+
+def reshape_obs(obs, image=True):
+    # in - N agents 3 21 21
     # out - agents N*3 21 21
-    
     chanels = obs.shape[-3]
     stack = obs.shape[0]
     size = obs.shape[-1]
-  #  print(obs.shape)
-    result  = []
+    result = []
     for i in range(obs.shape[1]):
-        result.append(obs[:,i].reshape(chanels*stack, size, size))
-       # print(obs[0,i,:,:,:].shape)
+        if image:
+            result.append(obs[:, i].reshape(chanels * stack, size, size))
+        else:
+            result.append(obs[:, i].reshape(chanels * stack * size * size))
     return np.asarray(result)
 
+
 class RavelFrameStack(gym.Wrapper):
-    def __init__(self, env):
+    def __init__(self, env, image_obs=True):
         super().__init__(env)
         self.frame_stack = self.env.observation_space.shape[0]
         self.agents = self.env.observation_space.shape[1]
-        self.observation_space = gym.spaces.Box(0.0, 1.0, shape=( 3*self.frame_stack,
-                                                                 self.env.observation_space.shape[-1],
-                                                                self.env.observation_space.shape[-1]))
-    
+        self.field_size = self.env.observation_space.shape[-1]
+        # print(self.env.observation_space.shape[-1])
+        self.image_obs = image_obs
+        if image_obs:
+            self.observation_space = gym.spaces.Box(0.0, 1.0, shape=(3 * self.frame_stack,
+                                                                     self.field_size,
+                                                                     self.field_size))
+        else:
+            self.observation_space = gym.spaces.Box(0.0, 1.0,
+                                                    shape=(1, 3 * self.frame_stack * self.field_size * self.field_size))
+
     def reset(self, **kwargs):
-        obs  = self.env.reset()
-        #print(np.asarray(obs).shape)
-        return reshape_obs(np.asarray(obs))
-    
+        obs = self.env.reset()
+        return reshape_obs(np.asarray(obs), image=self.image_obs)
+
     def step(self, action):
-      #  print(action)
         observations, reward, done, info = self.env.step(action)
-       # print(observations.shape)
-      #  print(self.observation_space.shape)
-        observations = reshape_obs(np.asarray(observations))
-       # print(observations.shape)
+        observations = reshape_obs(np.asarray(observations), image=self.image_obs)
         return observations, reward, done, info
-    
+
+
+class UseOneAgent(gym.Wrapper):
+    def reset(self, **kwargs):
+        return self.env.reset()[0]
+
+    def step(self, action):
+        observations, reward, done, info = self.env.step(action)
+        return observations[0], reward, done, info
+
+
+def init_imagebased_pogema(stack_len, radius, img_size, num_agents):
+    # Init image pogema
+    gc = GridConfig(seed=None, num_agents=num_agents, max_episode_steps=64, obs_radius=radius, size=16, density=0.3)
+    env = gym.make('POMAPF-v0', grid_config=gc, with_animations=True, auto_reset=False, egocentric_idx=None,
+                   observation_type='MAPF')
+    env = Obs2DActionWrapper(env)
+    env = FrameStack(env, num_stack=stack_len)
+    env = RavelFrameStack(env)
+    return env
+
+
+def init_vactorbased_pogema(stack_len, radius, img_size, num_agents, wandb = None):
+    # Init image pogema
+
+    gc = GridConfig(seed=None, num_agents=num_agents, max_episode_steps=64, obs_radius=radius, size=16, density=0.3)
+    env = gym.make('POMAPF-v0', grid_config=gc, with_animations=True, auto_reset=False, egocentric_idx=None,
+                   observation_type='MAPF')
+    env = Obs2DActionWrapper(env)
+    env = FrameStack(env, num_stack=stack_len)
+
+    env = RavelFrameStack(env, image_obs=False)
+    if isinstance(wandb, type(None)):
+        pass
+       # env = RewardLogger(env, wandb)
+    return env
+
+
+if __name__ == "__main__":
+    print("Test image wrappers!")
+    stack_len = 4
+    radius = 10
+    img_size = radius * 2 + 1
+    num_agents = 1
+
+    # Init image pogema
+    env = init_imagebased_pogema(stack_len, radius, img_size, num_agents)
+
+    obs = env.reset()
+    obs_ = np.asarray(obs)
+    assert (obs_.shape == (num_agents, 12, 21, 21))
+    print("2d <reset> observation is OK")
+
+    obs, _, _, _ = env.step(1)
+    obs_ = np.asarray(obs)
+    assert (obs_.shape == (num_agents, 12, 21, 21))
+    print("2d <step> observation is OK")
+    print()
+    # Init vector pogema
+    print("Test vector wrappers!")
+    env = init_vactorbased_pogema(stack_len, radius, img_size, num_agents)
+    obs = env.reset()
+    obs_ = np.asarray(obs)
+    assert (obs_.shape == (num_agents, 12 * 21 * 21))
+
+    print("1d <reset> observation is OK")
+    obs, _, _, _ = env.step(1)
+    obs_ = np.asarray(obs)
+    assert (obs_.shape == (num_agents, 12 * 21 * 21))
+    print("1d <step> observation is OK")
+    print("Act space: ", env.action_space)
+    env = UseOneAgent(env)
+    obs = env.reset()
+    obs_ = np.asarray(obs)
+    print(obs_.shape)
+    assert (obs_.shape == (12 * 21 * 21,))
+    print("1d <use one agent> observation is OK")
+
+    print("\n Test vector wrappers with 3 agent!")
+    stack_len = 4
+    radius = 10
+    img_size = radius * 2 + 1
+    num_agents = 3
+
+    env = init_vactorbased_pogema(stack_len, radius, img_size, num_agents)
+    obs = env.reset()
+    obs_ = np.asarray(obs)
+    assert (obs_.shape == (num_agents, 12 * 21 * 21))
+
+    print("1d <reset> observation is OK")
+    obs, _, _, _ = env.step([1,2,3])
+    obs_ = np.asarray(obs)
+    assert (obs_.shape == (num_agents, 12 * 21 * 21))
+    print(obs_.shape)
+    print("1d <step> observation is OK")
+    print("Act space: ", env.action_space)
