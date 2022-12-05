ad886c27e07537980501602a994398930f0d588b
babycar <zoya.v@ya.ru>
2022-12-05 16:55:08 +0300

    gen_pogema_traj_right

--- algorithms/generate_pogema_trajectories.py
+++ algorithms/generate_pogema_trajectories.py
@@ -2,18 +2,21 @@ from collections import defaultdict
 import numpy as np
 from sklearn.model_selection import train_test_split
 import d3rlpy
-
+from tqdm import tqdm
 from dt import discounted_cumsum
 from collections import Counter
+import gc
+
 
 def distance_to_goal(obs):
-    ax, ay = np.asarray(obs[1].shape)//2
-    #print(obs[2])
-    #exit()
+    ax, ay = np.asarray(obs[1].shape) // 2
+    # print(obs[2])
+    # exit()
     if np.sum(obs[2]) == 1:
-        x,y = np.where(obs[2]==1)
-        return ((ax-x)**2 + (ay-y)**2)**0.5
+        x, y = np.where(obs[2] == 1)
+        return ((ax - x) ** 2 + (ay - y) ** 2) ** 0.5
     return 1000
+
 
 def episode_rewards_by_obs(episode_observation):
     min_dist = 1000
@@ -26,71 +29,91 @@ def episode_rewards_by_obs(episode_observation):
             min_dist = dist
             reward = 0.1
         new_rewards.append(reward)
-    new_rewards[-1]=1
+    new_rewards[-1] = 1
+    # print(new_rewards)
+    #  exit()
     return new_rewards
 
+
 def load_pogema_trajectories(use_image=True):
-    dataset = d3rlpy.dataset.MDPDataset.load("./mixed_dataset_10_00_5000.h5")
-    train_episodes, test_episodes = train_test_split(dataset, test_size=0.2, shuffle=True)
+    dataset = d3rlpy.dataset.MDPDataset.load("../data/mixed_dataset_10_0_27910.h5")
+    dataset, test_episodes = train_test_split(dataset, test_size=0.3, shuffle=True)
     traj, traj_len = [], []
 
     data_, episode_step = defaultdict(list), 0
     episode_data = dict()
+    stack_len = 4
     traj = []
     traj_len = []
     full_data = {"observations": []}
     episode_data = dict()
-    for i in range(len(dataset)):
-        #mask = np.where(dataset[i].actions == 0)
+    for i in tqdm(range(len(dataset))):
+        # mask = np.where(dataset[i].actions == 0)
         mask = np.where(dataset[i].actions >= 0)
         # print("Zero indexes: ", mask)
         # print("Other: ", mask2)
         # print("-------------")
         # continue
+        #  print()
         episode = dataset[i]
         episode_len = len(episode.rewards[mask])
         if episode_len < 20: continue
         new_rewards = np.asarray(episode_rewards_by_obs(episode.observations))
-        for j in range(episode_len,20,-3):
-            i = j-20
+        for j in range(episode_len, 20, -3):
+            i = j - 20
             if use_image:
-                episode_data["observations"] = episode.observations[mask][i:i+20].reshape(-1, 12, 21, 21)
-                full_data["observations"] += episode.observations[mask][i:i+20].tolist()
+                episode_data["observations"] = episode.observations[mask][i:i + 20].reshape(-1, stack_len * 3, 21, 21)
+                full_data["observations"] += episode.observations[mask][i:i + 20].tolist()
             else:
-                episode_data["observations"] = episode.observations[mask][i:i+20].reshape(-1, 12 * 21 * 21)
-                full_data["observations"] += episode.observations[mask][i:i+20].reshape(-1, 12 * 21 * 21).tolist()
-            episode_data["actions"] = episode.actions[mask][i:i+20].reshape(-1, 1)
-            episode_data["rewards"] = new_rewards[mask][i:i+20]
+                episode_data["observations"] = episode.observations[mask][i:i + 20].reshape(-1, stack_len * 3 * 21 * 21)
+                full_data["observations"] += episode.observations[mask][i:i + 20].reshape(-1,
+                                                                                          stack_len * 3 * 21 * 21).tolist()
+            episode_data["actions"] = episode.actions[mask][i:i + 20].reshape(-1, 1)
+            episode_data["rewards"] = new_rewards[mask][i:i + 20]
             reward_mask = np.where(episode_data["actions"].ravel() == 0)
+            if len(reward_mask[0]) != 0:
+                #  print(reward_mask)
+                _mask = np.where(episode_data["actions"].ravel() != 0)
+            #  print(_mask)
+            #   print("-------------")
             episode_data["rewards"][reward_mask] = -0.05
-         #   episode_data["rewards"][-1] = 1.5
+            #   episode_data["rewards"][-1] = 1.5
             episode_data["returns"] = episode_data["rewards"]
-            episode_len = len(episode.rewards[mask][i:i+20])
+            episode_len = len(episode.rewards[mask][i:i + 20])
             traj.append(episode_data)
             traj_len.append(episode_len)
             episode_data = dict()
 
+    gc.collect()
     print("Trajectories statistics: ")
     print("Mean: ", np.mean(traj_len))
     print("Median: ", np.median(traj_len))
     print("Max: ", np.max(traj_len))
     print("Min: ", np.min(traj_len))
-    full_data["observations"] = np.asarray(full_data["observations"])
+   # full_data["observations"] = np.asarray(full_data["observations"])
+    print("Aboba")
+    mean_val = np.mean(full_data["observations"][:5])
+    std_val = np.mean(full_data["observations"][:5])
+    obs_shape = traj[0]['observations'].shape[1]
     info = {
-        "obs_mean": full_data["observations"].mean(0, keepdims=True),
-        "obs_std": full_data["observations"].std(0, keepdims=True) + 1e-6,
+        "obs_mean": np.asarray([mean_val for _ in range(obs_shape)]).reshape(1, -1),
+        "obs_std": np.asarray([std_val + 1e-6 for _ in range(obs_shape)]).reshape(1, -1),
         "traj_lens": np.array(traj_len),
     }
     return traj, info
 
 
 if __name__ == "__main__":
-    traj, _ = load_pogema_trajectories(use_image=False)
+    traj, info = load_pogema_trajectories(use_image=False)
     print(traj[0]['observations'].shape)
     print(traj[0]['actions'].shape)
     print(traj[0]['returns'].shape)
     print(traj[0]['rewards'].shape)
 
+    print(info['obs_mean'].shape)
+    print(info['traj_lens'].shape)
+    print(traj[1]['actions'])
+    print(traj[20]['actions'])
     from collections import Counter
 
     act = traj[0]['actions'].ravel().tolist()
